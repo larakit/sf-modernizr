130a1d56196706b870e039a53d226652e0769a3c
Alexey Berdnikov <aberdnikov@gmail.com>
2017-04-25 10:03:26 +0500

    fixed

---
+++ init.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by Larakit.
+ * Link: http://github.com/larakit
+ * User: Alexey Berdnikov
+ * Date: 20.04.17
+ * Time: 9:23
+ */
+\Larakit\StaticFiles\Manager::package('larakit/sf-modernizr')
+    ->setSourceDir('public')
+    ->jsPackage('modernizr.min.js');
